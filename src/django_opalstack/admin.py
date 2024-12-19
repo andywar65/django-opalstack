87e60c5cabbcffec3a7bdc954a4cd21124f35114
andywar65 <andy.war1965@gmail.com>
2024-12-19 20:00:44 +0100

    added token model

--- src/django_opalstack/admin.py
+++ src/django_opalstack/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
 
-# Register your models here.
+from .models import Token
+
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "key",
+    )
