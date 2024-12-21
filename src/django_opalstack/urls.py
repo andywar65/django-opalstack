519d7036f8b62d56767d685ea086e63ecb89eec3
andywar65 <andy.war1965@gmail.com>
2024-12-21 19:33:47 +0100

    added token detail view

--- src/django_opalstack/urls.py
+++ src/django_opalstack/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TokenListView
+from .views import TokenDetailView, TokenListView
 
 app_name = "django_opalstack"
 urlpatterns = [
     path("token/", TokenListView.as_view(), name="token_list"),
+    path("token/<pk>/", TokenDetailView.as_view(), name="token_detail"),
 ]
--- src/django_opalstack/views.py
+++ src/django_opalstack/views.py
@@ -1,8 +1,9 @@
 from typing import Any
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.db.models.query import QuerySet
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Token
 
@@ -18,4 +19,20 @@ class TokenListView(LoginRequiredMixin, ListView):
     def get_template_names(self):
         if "Hx-Request" in self.request.headers:
             return ["django_opalstack/htmx/token_list.html"]
+        return super().get_template_names()
+
+
+class TokenDetailView(LoginRequiredMixin, DetailView):
+    model = Token
+    template_name = "django_opalstack/token_detail.html"
+
+    def get_object(self):
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            raise PermissionDenied
+        return obj
+
+    def get_template_names(self):
+        if "Hx-Request" in self.request.headers:
+            return ["django_opalstack/htmx/token_detail.html"]
         return super().get_template_names()
