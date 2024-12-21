f8be3072215e6b67043f3b14b6f106e7cdca7025
andywar65 <andy.war1965@gmail.com>
2024-12-21 18:14:57 +0100

    adding token list view

--- src/django_opalstack/views.py
+++ src/django_opalstack/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render  # noqa
+from typing import Any
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models.query import QuerySet
+from django.views.generic import ListView
+
+from .models import Token
+
+
+class TokenListView(LoginRequiredMixin, ListView):
+    model = Token
+    template_name = "django_opalstack/token_list.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = Token.objects.filter(user=self.request.user)
+        return qs
