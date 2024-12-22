fa041fa4f1faad1c48cf861702c0016f3e3e63fa
andywar65 <andy.war1965@gmail.com>
2024-12-22 16:57:43 +0100

    added token users detail view

--- src/django_opalstack/urls.py
+++ src/django_opalstack/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import TokenDetailView, TokenListView, TokenServerDetailView
+from .views import (
+    TokenDetailView,
+    TokenListView,
+    TokenServerDetailView,
+    TokenUsersDetailView,
+)
 
 app_name = "django_opalstack"
 urlpatterns = [
     path("token/", TokenListView.as_view(), name="token_list"),
     path("token/<pk>/", TokenDetailView.as_view(), name="token_detail"),
     path("token/<pk>/servers/", TokenServerDetailView.as_view(), name="server_list"),
+    path("token/<pk>/users/", TokenUsersDetailView.as_view(), name="user_list"),
 ]
--- src/django_opalstack/views.py
+++ src/django_opalstack/views.py
@@ -52,3 +52,17 @@ class TokenServerDetailView(TokenDetailView):
         opalapi = opalstack.Api(token=self.object.key)
         context["web_servers"] = opalapi.servers.list_all()["web_servers"]
         return context
+
+
+class TokenUsersDetailView(TokenDetailView):
+
+    def get_template_names(self):
+        if "Hx-Request" not in self.request.headers:
+            raise Http404
+        return ["django_opalstack/htmx/users_list.html"]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        opalapi = opalstack.Api(token=self.object.key)
+        context["osusers"] = opalapi.osusers.list_all(embed=["server"])
+        return context
