f28ddb3e14b77c9e3772471470f7391102a82cab
andywar65 <andy.war1965@gmail.com>
2024-12-22 16:26:37 +0100

    adding token server detail view

--- src/django_opalstack/urls.py
+++ src/django_opalstack/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TokenDetailView, TokenListView
+from .views import TokenDetailView, TokenListView, TokenServerDetailView
 
 app_name = "django_opalstack"
 urlpatterns = [
     path("token/", TokenListView.as_view(), name="token_list"),
     path("token/<pk>/", TokenDetailView.as_view(), name="token_detail"),
+    path("token/<pk>/servers/", TokenServerDetailView.as_view(), name="server_list"),
 ]
--- src/django_opalstack/views.py
+++ src/django_opalstack/views.py
@@ -1,8 +1,10 @@
 from typing import Any
 
+import opalstack
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.db.models.query import QuerySet
+from django.http import Http404
 from django.views.generic import DetailView, ListView
 
 from .models import Token
@@ -36,3 +38,17 @@ class TokenDetailView(LoginRequiredMixin, DetailView):
         if "Hx-Request" in self.request.headers:
             return ["django_opalstack/htmx/token_detail.html"]
         return super().get_template_names()
+
+
+class TokenServerDetailView(TokenDetailView):
+
+    def get_template_names(self):
+        if "Hx-Request" not in self.request.headers:
+            raise Http404
+        return ["django_opalstack/htmx/server_list.html"]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        opalapi = opalstack.Api(token=self.object.key)
+        context["web_servers"] = opalapi.servers.list_all()["web_servers"]
+        return context
