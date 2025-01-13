e1cc8c00f96331313fee71e5871a867dddd0d711
andywar65 <andy.war1965@gmail.com>
2025-01-14 00:00:46 +0100

    added impostor login test

--- project/project/tests.py
+++ project/project/tests.py
@@ -15,6 +15,7 @@ class OpalstackViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         user = User.objects.create_superuser("boss", "test@example.com", "p4s5w0r6")
+        User.objects.create_superuser("impostor", "impostor@example.com", "p4s5w0r6")
         Token.objects.create(
             name="test_token",
             key=OPAL_KEY,
@@ -36,3 +37,76 @@ class OpalstackViewTest(TestCase):
             )
         )
         self.assertEqual(response.status_code, 302)
+        response = self.client.get(
+            reverse(
+                "django_opalstack:user_list",
+                kwargs={"pk": token.id},
+            )
+        )
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(
+            reverse(
+                "django_opalstack:app_list",
+                kwargs={"pk": token.id},
+            )
+        )
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(
+            reverse(
+                "django_opalstack:site_list",
+                kwargs={"pk": token.id},
+            )
+        )
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(
+            reverse(
+                "django_opalstack:app_detail",
+                kwargs={"pk": token.id},
+            )
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_impostor_status_code(self):
+        self.client.login(username="impostor", password="p4s5w0r6")
+        response = self.client.get(
+            reverse(
+                "django_opalstack:token_list",
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        token = Token.objects.get(name="test_token")
+        response = self.client.get(
+            reverse(
+                "django_opalstack:token_detail",
+                kwargs={"pk": token.id},
+            )
+        )
+        self.assertEqual(response.status_code, 403)
+        response = self.client.get(
+            reverse(
+                "django_opalstack:user_list",
+                kwargs={"pk": token.id},
+            )
+        )
+        self.assertEqual(response.status_code, 403)
+        response = self.client.get(
+            reverse(
+                "django_opalstack:app_list",
+                kwargs={"pk": token.id},
+            )
+        )
+        self.assertEqual(response.status_code, 403)
+        response = self.client.get(
+            reverse(
+                "django_opalstack:site_list",
+                kwargs={"pk": token.id},
+            )
+        )
+        self.assertEqual(response.status_code, 403)
+        response = self.client.get(
+            reverse(
+                "django_opalstack:app_detail",
+                kwargs={"pk": token.id},
+            )
+        )
+        self.assertEqual(response.status_code, 403)
